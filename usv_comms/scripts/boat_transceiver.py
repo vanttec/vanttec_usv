ba7e3ccd79278d881c42400e05b7d190c07f6235
vSebas <55563384+vSebas@users.noreply.github.com>
2020-01-30 16:27:32 -0600

    Update usv_comms/scripts/boat_transceiver.py
    Co-Authored-By: IvanaColl <43309523+IvanaColl@users.noreply.github.com>

--- usv_comms/scripts/boat_transceiver.py
+++ usv_comms/scripts/boat_transceiver.py
@@ -33,7 +33,6 @@ BAUD_RATE = 9600
 REMOTE_NODE_ID = "vtecstation"
 # Frequency in Hz in which the node will be run at.
 ROS_RATE = 100
-#**************************************************************************#
 
 
 class XbeeBoat:
