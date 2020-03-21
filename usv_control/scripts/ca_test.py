4fea441a4f4fffdeb1b352f1804a7393968c8d5f
IvanaColl <ivanacollado@gmail.com>
2020-03-21 12:13:24 -0600

    Change Lidar obstacle message

--- usv_control/scripts/ca_test.py
+++ usv_control/scripts/ca_test.py
@@ -4,8 +4,8 @@ import os
 import time
 import rospy
 from std_msgs.msg import Float32MultiArray
-from usv_perception.msg import obj_detected
-from usv_perception.msg import obj_detected_list
+from geometry_msgs.msg import Vector3
+from usv_perception.msg import obstacles_list
 import math
 
 #Constant Speed and Constant angular
