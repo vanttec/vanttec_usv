a69a88c345fa7827a72647b02e2b223d3d22cb9b
Alejandro Gonzalez <33239173+alexglzg@users.noreply.github.com>
2020-05-09 10:48:53 -0500

    Add files via upload

--- usv_control/scripts/lidar_obstacle_simulator.py
+++ usv_control/scripts/lidar_obstacle_simulator.py
@@ -40,7 +40,7 @@ class ObstacleSimulator:
         self.ned_y = 0
         self.yaw = 0
 
-        self.challenge = 0 #1 for AutonomousNavigation, 2 for SpeedChallenge
+        self.challenge = 1 #1 for AutonomousNavigation, 2 for SpeedChallenge
         self.obstacle_list = []
 
         self.max_visible_radius = 100
@@ -136,7 +136,7 @@ class ObstacleSimulator:
         marker_array = MarkerArray()
         for i in range(len(self.obstacle_list)):
             x = self.obstacle_list[i]['X']
-            y = self.obstacle_list[i]['Y']
+            y = -self.obstacle_list[i]['Y']
             radius = self.obstacle_list[i]['R']
             marker = Marker()
             marker.header.frame_id = "/world"
@@ -164,13 +164,13 @@ def main():
     obstacleSimulator = ObstacleSimulator()
     if obstacleSimulator.challenge == 0:
         obstacleSimulator.obstacle_list.append({'X' : 3.0,
-                                    'Y' : 0.0,
+                                    'Y' : -0.1,
                                     'R' : 0.5})
-        
+        '''
         obstacleSimulator.obstacle_list.append({'X' : 7.0,
                                     'Y' : 0.4,
                                     'R' : 0.5})
-        '''
+        
         obstacleSimulator.obstacle_list.append({'X' : 7.0,
                                     'Y' : 0.2,
                                     'R' : 0.2})
@@ -180,78 +180,66 @@ def main():
                                     'R' : .2})
         '''
     elif obstacleSimulator.challenge == 1:
-        obstacleSimulator.obstacle_list.append({'X' : 3.0,
+        obstacleSimulator.obstacle_list.append({'X' : 3.1,
                                     'Y' : 1.1,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 3.0,
-                                    'Y' : 0.762,
+        obstacleSimulator.obstacle_list.append({'X' : 3.3,
+                                    'Y' : 2.2,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 3.0,
-                                    'Y' : -0.762,
+        obstacleSimulator.obstacle_list.append({'X' : 3.2,
+                                    'Y' : -3.3,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 4.524,
-                                    'Y' : -0.762,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 4.524,
-                                    'Y' : -2.186,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 4.762,
-                                    'Y' : -2.5,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 4.8,
-                                    'Y' : -1.6,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 5.2,
-                                    'Y' : -2.186,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 5.4,
-                                    'Y' : -2.5,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 5.8,
-                                    'Y' : -1.6,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.3,
-                                    'Y' : -1.6,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.6,
-                                    'Y' : -0.762,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.8,
-                                    'Y' : .2,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.95,
-                                    'Y' : 0.762,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 7.6,
-                                    'Y' : .5,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 7.20,
-                                    'Y' : 1,
-                                    'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.6,
+        obstacleSimulator.obstacle_list.append({'X' : 6.2,
                                     'Y' : 1.2,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.0,
-                                    'Y' : 1.5,
+        obstacleSimulator.obstacle_list.append({'X' : 4.1,
+                                    'Y' : -4.2,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 5.5,
-                                    'Y' : 1.8,
+        obstacleSimulator.obstacle_list.append({'X' : 4.4,
+                                    'Y' : -2.5,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 4.5,
-                                    'Y' : 1.3,
+        obstacleSimulator.obstacle_list.append({'X' : 5.3,
+                                    'Y' : -3.4,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 4.0,
-                                    'Y' : 1.5,
+        obstacleSimulator.obstacle_list.append({'X' : 6.2,
+                                    'Y' : 2.3,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 3.6,
-                                    'Y' : 1.6,
+        obstacleSimulator.obstacle_list.append({'X' : 9.1,
+                                    'Y' : -4.4,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 3.5,
-                                    'Y' : 1.1,
+        obstacleSimulator.obstacle_list.append({'X' : 9.6,
+                                    'Y' : -3.6,
                                     'R' : 0.105})
-        obstacleSimulator.obstacle_list.append({'X' : 6.048,
-                                    'Y' : 0.1,
-                                    'R' : 0.127})
+        obstacleSimulator.obstacle_list.append({'X' : 12.6,
+                                    'Y' : 3.4,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 10.7,
+                                    'Y' : -4.6,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 10.1,
+                                    'Y' : 5.5,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 9.3,
+                                    'Y' : 4.1,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 12.4,
+                                    'Y' : -3.3,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 13.6,
+                                    'Y' : -1.6,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 13.4,
+                                    'Y' : 2.4,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 14.2,
+                                    'Y' : -0.2,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 5.1,
+                                    'Y' : 4.2,
+                                    'R' : 0.105})
+        obstacleSimulator.obstacle_list.append({'X' : 7.7,
+                                    'Y' : 5.6,
+                                    'R' : 0.105})
 
 
 
