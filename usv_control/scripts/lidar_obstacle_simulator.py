0c12b50ac240db8bb73ec1c8126e6a09f04a3209
vSebas <sebas.martp@gmail.com>
2021-05-06 23:34:13 -0500

    Update obstacles and waypoints

--- usv_control/scripts/lidar_obstacle_simulator.py
+++ usv_control/scripts/lidar_obstacle_simulator.py
@@ -170,18 +170,14 @@ def main():
                                     'Y' : -3.0,
                                     'R' : 1.5})
 
-        # obstacleSimulator.obstacle_list.append({'X' : 5.0,
-        #                             'Y' : -1.0,
-        #                             'R' : 0.2})
-
         obstacleSimulator.obstacle_list.append({'X' : 7.0,
                                     'Y' : -6.0,
                                     'R' : 0.5})
-        '''
-        obstacleSimulator.obstacle_list.append({'X' : 10.0,
-                                    'Y' : 0.0,
-                                    'R' : 0.2})
-        '''
+
+        obstacleSimulator.obstacle_list.append({'X' : 5.5,
+                                    'Y' : -3.0,
+                                    'R' : 1})
+
     elif obstacleSimulator.challenge == 1:
 
         obstacleSimulator.obstacle_list.append({'X' : 3.1,
