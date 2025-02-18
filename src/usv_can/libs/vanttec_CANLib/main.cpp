19f1b406ba9195b77a6c1b22ed072423cb2a390c
DemianMArin <demianmarin03@gmail.com>
2025-02-17 18:17:47 -0600

    Test CAN with joy

--- src/usv_can/libs/vanttec_CANLib/main.cpp
+++ src/usv_can/libs/vanttec_CANLib/main.cpp
@@ -10,7 +10,8 @@
 #include "Vanttec_CANLib_Linux/CANHandler.h"
 
 int main() {
-    vanttec::CANHandler handler("can_vtec");
+    // vanttec::CANHandler handler("can_vtec");
+  vanttec::CANHandler handler("can0");
 //    std::thread writeThread([&]{
 //        for(;;) {
 //            handler.write(vanttec::CANMessage());
