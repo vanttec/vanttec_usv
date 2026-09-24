94d3abe35cf1856a02d9b47ac54d76b7761a34b3
Carlos Reyes <carlosrgb06@gmail.com>
2026-09-23 20:31:20 -0600

    custom msg UsvLoc was moved to usv_interfaces | UsvLoc now publishes roll and pitch

--- src/usv_localization/src/localization.cpp
+++ src/usv_localization/src/localization.cpp
@@ -6,7 +6,7 @@
 #include "sbg_driver/msg/sbg_ekf_nav.hpp"
 #include "tf2/utils.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "usv_localization/msg/usv_loc.hpp"
+#include "usv_interfaces/msg/usv_loc.hpp"
 
 class Localization : public rclcpp::Node {
 public:
@@ -17,7 +17,7 @@ public:
     subscription_nav = this->create_subscription<sbg_driver::msg::SbgEkfNav>(
       "/sbg/ekf_nav", 10, std::bind(&Localization::nav_callback, this, std::placeholders::_1));
 
-    publisher = this->create_publisher<usv_localization::msg::UsvLoc>("/usv_loc", 10);
+    publisher = this->create_publisher<usv_interfaces::msg::UsvLoc>("/usv_loc", 10);
 
     RCLCPP_INFO(this->get_logger(), "Nodo de Localization iniciado.");
   }
@@ -25,10 +25,11 @@ public:
 private:
   rclcpp::Subscription<sbg_driver::msg::SbgEkfQuat>::SharedPtr subscription_quat;
   rclcpp::Subscription<sbg_driver::msg::SbgEkfNav>::SharedPtr subscription_nav;
-  rclcpp::Publisher<usv_localization::msg::UsvLoc>::SharedPtr publisher;
+  rclcpp::Publisher<usv_interfaces::msg::UsvLoc>::SharedPtr publisher;
 
   bool origin_set = false;
-  bool yaw_valid = false;
+  bool angles_valid = false;
+  bool pose_valid = false;
   double lat0 = 0.0;
   double lon0 = 0.0;
   double cos_lat0 = 0.0;
@@ -55,7 +56,7 @@ private:
 
   void quat_callback(const sbg_driver::msg::SbgEkfQuat::SharedPtr msg) {
 
-    yaw_valid = (msg->status.solution_mode == 4) && (msg->status.heading_valid);
+    angles_valid = (msg->status.solution_mode == 4) && (msg->status.heading_valid) && (msg->status.attitude_valid);
 
     tf2::Quaternion q(
       msg->quaternion.x,
@@ -66,11 +67,12 @@ private:
 
     tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
 
-    if (!yaw_valid) {
+    if (!angles_valid) {
      RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 5000,
-        "DATOS NO CONFIABLES - Modo: %d | Heading: %s | Mag: %s",
+        "DATOS NO CONFIABLES - Modo: %d | Attitude: %s | Heading: %s |Mag: %s",
         msg->status.solution_mode,
-        msg->status.heading_valid ? "OK" : "INVALID",
+        msg->status.attitude_valid ? "VALID" : "INVALID",
+        msg->status.heading_valid ? "VALID" : "INVALID",
         msg->status.mag_ref_used ? "USED" : "NOT USED");
     }
 
@@ -89,16 +91,31 @@ private:
         }
         return;
     }
+
+    pose_valid = (msg->status.solution_mode == 4) && (msg->status.position_valid);
+
+    if (!pose_valid) {
+     RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 5000,
+        "DATOS NO CONFIABLES - Mode: %d | Position: %s",
+        msg->status.solution_mode,
+        msg->status.position_valid ? "VALID" : "INVALID");
+    }
+
     NedPosition pos = latLonToNED(msg->latitude,msg->longitude);
 
-    usv_localization::msg::UsvLoc msg_out;
+    usv_interfaces::msg::UsvLoc msg_out;
     msg_out.status = msg->status.solution_mode;
     msg_out.latitud = msg->latitude;
     msg_out.longitud = msg->longitude;
     msg_out.position_valid = msg->status.position_valid;
     msg_out.x = pos.x;
     msg_out.y = pos.y;
-    msg_out.heading_valid = yaw_valid;
+
+
+    msg_out.attitude_valid= msg->status.attitude_valid;
+    msg_out.heading_valid = msg->status.heading_valid;
+    msg_out.roll = roll;
+    msg_out.pitch = pitch;
     msg_out.yaw = yaw;
 
     publisher->publish(msg_out);
